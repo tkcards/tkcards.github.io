e21149ff28782608c443da3e1ee6454028299c2b
tkoli <tkoli@umich.edu>
2024-10-23 23:07:12 -0500

    game loop and css

--- scripts/page.js
+++ scripts/page.js
@@ -1,4 +1,57 @@
+// Game data
+const games = [
+    {
+        name: "King's Cup",
+        image: "src/squirrel.png",
+        alt: "kcup image",
+        content: "kcupkcup"
+    },
+    {
+        name: "Pablo",
+        image: "src/squirrel.png",
+        alt: "pablo image",
+        content: "PABLO TEST TEXT"
+    },
+    
+];
+
+// Creates games
+function createGames() {
+    // Games container
+    const games_container = $(".games_container");
+
+    games.forEach(game => {
+        // Create game
+        const gameDiv = $('<div class="game"></div>');
+
+        // Button
+        const button = $(`
+            <button class="game_button">
+            <img src="${game.image}" alt="${game.alt}" class="game_image">
+            <div class="game_title">${game.name}</div>
+            </button>
+        `);
+
+        // Content
+        const contentDiv = $(`<div class="game_content" style="display: none;">${game.content}</div>`);
+
+        // Display game
+        gameDiv.append(button);
+        gameDiv.append(contentDiv);
+        games_container.append(gameDiv);
+    
+        // Select game
+        button.click(() => {
+            contentDiv.toggle(); // Toggle the content visibility on button click
+        });
+    });
+}
+
+// On page load 
 $(document).ready(function () {
+    createGames(); // Create game elements dynamically
+
+    /*
     var pablo_button = $('.pablo_button');
     var kcup_button = $('.kcup_button');
     var pablo_content = $('.pablo_content');
@@ -22,4 +75,5 @@ $(document).ready(function () {
             kcup_content.show();
         }
     });
+    */
 });
